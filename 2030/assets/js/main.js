fe0f896e41aca4cac31fc88464b8537e933f1543
AmandaTwigg <twiggmandy2@gmail.com>
2020-06-26 01:12:07 +1000

    2030 and EVPS

--- 2030/assets/js/main.js
+++ 2030/assets/js/main.js
@@ -1,17 +1,34 @@
 $(document).ready(function(){
 	console.log("I'm Ready!");
 
-	$('.h-1').click(function(){
-		$('.t-1').slideToggle("fast");
+	$(".h-1").click(function(){
+		$(".t-1").slideToggle("fast"); 
 	});
 
-	$('.h-2').click(function(){
-		$('.t-2').slideToggle("fast");
+	$(".h-1").click(function(){
+		$(".t-1").slideToggle("fast"); 
 	});
 	
-	$('.h-3').click(function(){
-		$('.t-3').slideToggle("fast");
-	});
+	$(".header").click(function(){
+		$(".toggle").slideToggle("fast"); 
+	});		
+});
 
-}); 
 
+
+window.onload = function() {
+
+    //add constraints object
+    var constraints = { audio: true,
+                        video: true};
+
+    //call getUserMedia, then the magic
+    navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+        var video = document.querySelector('video');
+        video.srcObject = mediaStream;
+        video.play();
+    }).catch(function(err){
+        console.log("yikes, and err!" + err.message);
+    });
+
+}
